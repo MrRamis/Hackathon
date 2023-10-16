7fd56c8e1422c0551b5570989300af568c9e1d05
MrRamis <99258165+MrRamis@users.noreply.github.com>
2023-10-17 00:26:13 +0300

    Merge branch 'master' of https://github.com/MrRamis/Hackathon

--- app/src/main/java/com/example/hackathon/view/ViewGetBookActivity.kt
+++ app/src/main/java/com/example/hackathon/view/ViewGetBookActivity.kt
@@ -1,6 +1,60 @@
 package com.example.hackathon.view
 
+import android.annotation.SuppressLint
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.hackathon.ui.theme.HackathonTheme
 
 class ViewGetBookActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HackathonTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    DefaultPreview()
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun GetBookActivity() {
+Column() {
+    Text(text = "название")
+    Column() {
+        Text(text = "год")
+        Text(text = "тип")
+    }
+    
+}
 }
