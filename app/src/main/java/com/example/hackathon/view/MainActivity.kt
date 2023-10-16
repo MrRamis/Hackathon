6876a1b2c1514859d773c8b7dd4de59a2f7d3a11
BladeRunner <BladeRunner@DESKTOP-NTANRSG>
2023-10-16 18:46:54 +0300

    commit message from ulya

--- app/src/main/java/com/example/hackathon/view/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/view/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.hackathon.view
+//package com.example.helloapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,40 +9,46 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hackathon.ui.theme.HackathonTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var test = 0
+        //var test = 0
         setContent {
+            Scaffold(
+                Text("Hello METANIT.COM", fontSize = 28.sp)
+            )
+        }
+
+
+
+
+        @Composable
+        fun Greeting(name: String, modifier: Modifier = Modifier) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+
+       // @Preview(showBackground = true)
+        @Composable
+        fun GreetingPreview() {
             HackathonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Hakathon!! It's us")
-                }
+                Greeting("Android")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HackathonTheme {
-        Greeting("Android")
     }
 }
