5604457ed5d01b016dd22d079f43076ccb542d77
dantalyan <serafimraser@gmail.com>
2023-10-16 19:38:04 +0300

    table insert successfully

---
+++ app/src/main/java/com/example/hackathon/model/DB/Enties/SimpleTagsBooks.kt
@@ -0,0 +1,9 @@
+package com.example.hackathon.model.DB.Enties
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "SimpleTagsBooks")
+data class SimpleTagsBooks (@ColumnInfo(name = "Id_Author") var id_author : Int,
+                            @ColumnInfo(name = "Id_Tag") var id_tag : Int)
---
+++ app/src/main/java/com/example/hackathon/model/DB/Enties/StateBook.kt
@@ -0,0 +1,8 @@
+package com.example.hackathon.model.DB.Enties
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import com.example.hackathon.model.DB.State
+
+@Entity("Status")
+data class StateBook (@ColumnInfo(name = "Id_Book") var id_book : Int, @ColumnInfo(name = "status") var status : State)
