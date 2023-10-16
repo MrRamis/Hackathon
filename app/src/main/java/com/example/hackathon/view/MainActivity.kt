ec409606a58f73882817f024fe65a093adba204e
Andrey Shakhov <salfodias2003@gmail.com>
2023-10-16 19:42:01 +0300

    scuffold

--- app/src/main/java/com/example/hackathon/view/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/view/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.hackathon.view
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hackathon.ui.theme.HackathonTheme
+
+import androidx.compose.material.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,16 +43,74 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    HackathonTheme {
+//        Greeting("Android")
+//    }
+//}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun MyScreenContent() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "My App")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* Handle navigation icon click */ }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                }
+            )
+        },
+        content = {
+            Column {
+                Greeting("Hello")
+                Divider()
+                NameList(names = listOf("Alice", "Bob", "Charlie"))
+            }
+        }
+    )
+}
+@Composable
+fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        style = MaterialTheme.typography.headlineLarge,
+        modifier = Modifier.padding(16.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HackathonTheme {
-        Greeting("Android")
+fun NameList(names: List<String>) {
+    Column {
+        for (name in names) {
+            Text(text = name, modifier = Modifier.padding(16.dp))
+        }
     }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyScreenContent()
 }
