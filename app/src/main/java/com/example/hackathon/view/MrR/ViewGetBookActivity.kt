8f8f008ab202014794da8b987b6b2b003aa0afbc
dantalyan <serafimraser@gmail.com>
2023-10-17 21:49:01 +0300

    no correct

--- app/src/main/java/com/example/hackathon/view/MrR/ViewGetBookActivity.kt
+++ app/src/main/java/com/example/hackathon/view/MrR/ViewGetBookActivity.kt
@@ -20,17 +20,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hackathon.ui.theme.HackathonTheme
 import com.example.hackathon.viewModel.Book
-import com.example.hackathon.viewModel.ViewModelGetBook
+//import com.example.hackathon.viewModel.ViewModelGetBook
 
 class ViewGetBookActivity(): ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         val viewModelGetBook = ViewModelGetBook(Book(1,"Ведьмаг дикая охота","«Сага о ведьмаке» — цикл книг польского писателя Анджея Сапковского в жанре фэнтези.\n" +
-                 "\n" +
-                 "Первый рассказ цикла увидел свет в 1986 году, а последняя книга — в 2013. Действие книг происходит в вымышленном Мире «Ведьмака», напоминающем Европу времён позднего средневековья, где рядом с людьми существуют разного рода волшебные существа и чудовища.\n" +
-                 "\n" +
-                 "Геральт из Ривии — один из последних «ведьмаков», бродячих охотников на чудовищ","Книга","2015")
-         )
+//         val viewModelGetBook = ViewModelGetBook(Book(1,"Ведьмаг дикая охота","«Сага о ведьмаке» — цикл книг польского писателя Анджея Сапковского в жанре фэнтези.\n" +
+//                 "\n" +
+//                 "Первый рассказ цикла увидел свет в 1986 году, а последняя книга — в 2013. Действие книг происходит в вымышленном Мире «Ведьмака», напоминающем Европу времён позднего средневековья, где рядом с людьми существуют разного рода волшебные существа и чудовища.\n" +
+//                 "\n" +
+//                 "Геральт из Ривии — один из последних «ведьмаков», бродячих охотников на чудовищ","Книга","2015")
+//         )
         setContent {
             HackathonTheme {
                 Surface(
@@ -38,14 +38,14 @@ class ViewGetBookActivity(): ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    GetBookActivity(viewModelGetBook)
+                    //GetBookActivity(viewModelGetBook)
                 }
             }
         }
     }
      fun onCreate(savedInstanceState: Bundle?, book: Book) {
         super.onCreate(savedInstanceState)
-        val viewModelGetBook = ViewModelGetBook(book)
+        //val viewModelGetBook = ViewModelGetBook(book)
         setContent {
             HackathonTheme {
                 Surface(
@@ -53,7 +53,7 @@ class ViewGetBookActivity(): ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-                    GetBookActivity(viewModelGetBook)
+                   // GetBookActivity(viewModelGetBook)
                 }
             }
         }
@@ -62,33 +62,33 @@ class ViewGetBookActivity(): ComponentActivity() {
 
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun GetBookActivity(viewModel: ViewModelGetBook) {
-Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally ) {
-    val vm by viewModel.book.collectAsState()
-    Text(text = vm.name)
-    Row(modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceAround
-    ) {
-        Text(text = vm.date)
-        Text(text = vm.type)
-    }
-    Text(text = vm.describe)
-
-    
-}
-}
+//@Composable
+//fun GetBookActivity(viewModel: ViewModelGetBook) {
+//Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally ) {
+//    val vm by viewModel.book.collectAsState()
+//    Text(text = vm.name)
+//    Row(modifier = Modifier.fillMaxWidth(),
+//        horizontalArrangement = Arrangement.SpaceAround
+//    ) {
+//        Text(text = vm.date)
+//        Text(text = vm.type)
+//    }
+//    Text(text = vm.describe)
+//
+//
+//}
+//}
 
 
 @Preview
-@OptIn(ExperimentalMaterial3Api::class)
+//@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GetBookActivityPrevew(){
-    val viewModelGetBook = ViewModelGetBook(Book(1,"Ведьмаг дикая охота","«Сага о ведьмаке» — цикл книг польского писателя Анджея Сапковского в жанре фэнтези.\n" +
+    /*val viewModelGetBook = ViewModelGetBook(Book(1,"Ведьмаг дикая охота","«Сага о ведьмаке» — цикл книг польского писателя Анджея Сапковского в жанре фэнтези.\n" +
             "\n" +
             "Первый рассказ цикла увидел свет в 1986 году, а последняя книга — в 2013. Действие книг происходит в вымышленном Мире «Ведьмака», напоминающем Европу времён позднего средневековья, где рядом с людьми существуют разного рода волшебные существа и чудовища.\n" +
             "\n" +
             "Геральт из Ривии — один из последних «ведьмаков», бродячих охотников на чудовищ","Книга","2015")
-    )
-    GetBookActivity(viewModelGetBook)
+    )*/
+    //GetBookActivity(viewModelGetBook)
 }
